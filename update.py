e40d528fb1e1c2e2644f915b63cd56e46ff26ad6
AbdulRahman  Surakat <abdulrahmansurakat@gmail.com>
2026-02-04 12:28:51 +0000

    Add files via upload

---
+++ update.py
@@ -0,0 +1,19 @@
+import datetime
+import time
+
+target_datetime = datetime.datetime(2026, 2, 4, 12, 28, 0)
+
+while True:
+    now = datetime.datetime.now()
+    remaining = target_datetime - now 
+    
+    if remaining.total_seconds() <= 0:
+        print("Target reached")
+        break
+    
+    days = remaining.days
+    hours, rem = divmod(remaining.seconds, 3600)
+    minutes, seconds =divmod(rem, 60)
+    
+    print(F"{days}d, {hours}h, {minutes}m, {seconds}s")
+    time.sleep(1)
